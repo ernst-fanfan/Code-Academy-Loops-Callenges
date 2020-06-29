549db1618ff28b3b984d74c6e1c4624a9a2da0ef
Ernst Fanfan <11448222+ernst-fanfan@users.noreply.github.com>
2020-06-29 14:00:05 -0400

    Update c6.py
    made the code smaller

--- c6.py
+++ c6.py
@@ -1,8 +1,7 @@
 #Write your function here
 def larger_sum(lst1, lst2):
   largest = lst1
-  t1, t2 = sum(lst1), sum(lst2)
-  if (t2 > t1):
+  if (sum(lst2) > sum(lst1)):
     largest = lst2
   return largest
 #Uncomment the line below when your function is done
