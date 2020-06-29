a55451acc114196c0f127aea934417ee820ae928
Ernst Fanfan <11448222+ernst-fanfan@users.noreply.github.com>
2020-06-29 14:30:33 -0400

    Update c8.py
    Made the code smaller

--- c8.py
+++ c8.py
@@ -1,9 +1,6 @@
 #Write your function here
 def max_num(nums):
-  largest = nums[0]
-  for num in nums:
-    if (num > largest):
-      largest = num
-  return largest
+  nums.sort()
+  return nums[-1]
 #Uncomment the line below when your function is done
 print(max_num([50, -10, 0, 75, 20]))
