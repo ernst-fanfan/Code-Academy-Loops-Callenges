671676b01b9516afabc0203b3dc9fc1a686db168
Ernst Fanfan <11448222+ernst-fanfan@users.noreply.github.com>
2020-06-29 15:24:17 -0400

    Create c10.py

---
+++ c10.py
@@ -0,0 +1,8 @@
+#Write your function here
+def reversed_list(lst1, lst2):
+  lst2.reverse()
+  return lst1 == lst2
+
+#Uncomment the lines below when your function is done
+print(reversed_list([1, 2, 3], [3, 2, 1]))
+print(reversed_list([1, 5, 3], [3, 2, 1]))
